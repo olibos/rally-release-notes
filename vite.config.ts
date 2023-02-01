facc0f1cfc5f26bdd7bcb705c11406d641a60791
Olivier Bossaer <olivier.bossaer@gmail.com>
2023-02-01 21:22:43 +0100

    - Remove cache buster in file names - Deploy on release

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,15 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build:{
+    rollupOptions:
+    {
+      output:{
+        entryFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+      },
+    },
+  },
   resolve:{
     alias: [
       {
